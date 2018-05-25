66b292517b1abcd487d0c09aed0b6098ece76908
ryanmarcwegmeier <ryan.wegmeier@gmail.com>
2018-05-26 00:56:00 +0200

    Components for layour and first view

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './views/App';
+import HelloWorld from './views/HelloWorld';
+import User from './views/User';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((
+    <BrowserRouter>
+        <div>
+            <Route exact path="/" component={App} />
+            <Route exact path="/index" component={App} />
+            <Route path="/hello" component={HelloWorld} />
+            <Route path="/user" component={User} />
+        </div>
+    </BrowserRouter>
+    ), document.getElementById('root')
+);
 registerServiceWorker();
