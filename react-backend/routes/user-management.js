50ba3fc0432711f2460ef78bbc99ab661b3881ae
Ole Fass <jan.ole.fass@gmail.com>
2018-05-30 20:05:46 +0200

    some changes

--- react-backend/routes/user-management.js
+++ react-backend/routes/user-management.js
@@ -1,14 +1,6 @@
-// EXPRESS
-var express = require('express');
-var app = express();
-//app.use(express.static(__dirname));
-
 // parse application/x-www-form-urlencoded
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// set environment variable to override port
-app.set('port', process.env.PORT || 3000);
 
 // MONGODB
 var mongoose = require('mongoose');
@@ -25,13 +17,12 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 // a schema represents a collection and defines the shape of a document in that collection
 var Schema = mongoose.Schema;
 var userSchema = new Schema({
-    userName: String,
-    firstName: String,
-    lastName: String,
-    telNr: Number,
+    username: String,
+    firstname: String,
+    lastname: String,
+    telnr: Number,
     email: String,
-    password: String,
-    role: String
+    password: String
 });
 // convert schema to model. instances of models are documents
 var userModel = mongoose.model('User', userSchema);
@@ -47,12 +38,13 @@ app.post('/createUser', (req, res) => {
         if (err) {
             if(err.code === 11000) {
                 console.log("User already exists");
+                res.send('ERROR');
             } else console.log(err);
         } else {
             console.log("saved : " + JSON.stringify(user));
+            res.send('OK');
         }
-    });
-    res.redirect('/readAllUsers');
+    });    
 });
 
 /*
@@ -63,15 +55,15 @@ app.post("/readUser", (req, res, next) => {
     userModel.findById(req.body.id, (err, user) => {
         if(err) {
             console.log(err);
+            res.send('ERROR');
         }
         res.render('showUser', {
             title: 'Details for User: ' + user.userName,
-            userName: user.userName,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            telNr: user.telNr,
-            email: user.email,
-            role: user.role
+            username: user.username,
+            firstname: user.firstname,
+            lastname: user.lastname,
+            telnr: user.telnr,
+            email: user.email
             //password: user.password
         });
     });
@@ -84,6 +76,7 @@ app.get("/readAllUsers", (req, res, next) => {
     userModel.find({}, (err, user) => {
         if(err) {
             console.log(err);
+            res.send('ERROR');
         }
 
         var users = user.map((user) => {
@@ -100,10 +93,10 @@ app.post("/updateUser", (req, res, next) => {
     userModel.findByIdAndUpdate(req.body.description, req.body, (err, user) => {
         if(err) {
             console.log(err);
+            res.send('ERROR');
         }
     });
-    //TODO: post id
-    res.redirect('/readUser');
+    res.send('OK');
 });
 
 /*
@@ -114,14 +107,10 @@ app.post("/deleteUser", (req, res, next) => {
     userModel.findByIdAndRemove(req.body.id, (err) => {
         if (err) {
             console.log(err);
+            res.send('ERROR');
         } else {
-            console.log("user " + req.body.userName + " removed");
+            console.log("user " + req.body.username + " removed");
         }
     });
-    res.redirect('/readAllUsers');
+    res.send('OK');
 });
-
-//
-app.listen(app.get('port'), () => {
-    console.log( 'Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate...' );
-})
